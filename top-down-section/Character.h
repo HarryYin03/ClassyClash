a671eefcca57cf971fd8f7f9439d1b2fd8bb9d14
HarryYin03 <u6511238@au.edu>
2025-03-08 10:48:06 +0700

    undo movement not let charcter to move outside of the map

--- top-down-section/Character.h
+++ top-down-section/Character.h
@@ -3,13 +3,13 @@
 class Character
 {
 public:
-    Character();
+    Character(int winWidth, int winHeight);
     Vector2 getWorldPos()
     {
         return worldPos;
     };
-    void setScreenPos(int winWidth, int winHeight);
     void tick(float deltaTime);
+    void undoMovement();
 
 private:
     Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
@@ -17,6 +17,7 @@ private:
     Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
     Vector2 screenPos{};
     Vector2 worldPos{};
+    Vector2 worldPosLastFrame{};
     // 1: facing right, -1 : facing left
     float rightLeft{1.f};
     // animation variables
@@ -28,4 +29,5 @@ private:
     const float speed{4.f};
     float width{};
     float height{};
+    float scale{4.0f};
 };
--- top-down-section/main.cpp
+++ top-down-section/main.cpp
@@ -1,57 +1,18 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "character.h"
-{
-    Vector2 direction{};
-    if (IsKeyDown(KEY_A))
-        direction.x -= 1.0;
-    if (IsKeyDown(KEY_D))
-        direction.x += 1.0;
-    if (IsKeyDown(KEY_W))
-        direction.y -= 1.0;
-    if (IsKeyDown(KEY_S))
-        direction.y += 1.0;
-    if (Vector2Length(direction) != 0.0)
-    {
-        // set worldPos = worldPos + direction
 
-        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
-        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f; // if direction.x < 0, then knight is facing left
-        texture = run;
-    }
-    else
-    {
-        texture = idle;
-    }
-    // update the animation
-    runningTime += deltaTime;
-    if (runningTime >= updateTime)
-    {
-        
-        frame++;
-        runningTime = 0.f;
-        if (frame > maxFrames)
-        {
-            frame = 0;
-        }
-    }
-    // draw the character
-    Rectangle source{frame * (float)texture.width / 6.f, 0.f, rightLeft * (float)texture.width / 6.f, (float)texture.height};
-    Rectangle dest{screenPos.x, screenPos.y, 4.0f * (float)texture.width / 6.0f, 4.0f * (float)texture.height};
 
-    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
-}
-
-int main(void)
+int main()
 {
     const int windowWidth = 384;
     const int windowHeight = 384;
     InitWindow(windowWidth, windowHeight, "George's Top Down");
     Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
     Vector2 mapPos = {0.0, 0.0};
+    const float mapScale{4.0f};
 
-    Character knight;
-    knight.setScreenPos(windowWidth, windowHeight);
+    Character knight{windowWidth, windowHeight};
 
     SetTargetFPS(60);
 
@@ -62,10 +23,18 @@ int main(void)
 
         mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
 
-        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+        DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
         knight.tick(GetFrameTime());
+        if(knight.getWorldPos().x < 0.f ||
+        knight.getWorldPos().y < 0.f ||
+        knight.getWorldPos().x + windowWidth > map.width * mapScale||
+        knight.getWorldPos().y + windowHeight > map.height * mapScale )
+        {
+            knight.undoMovement();
+        }
 
         EndDrawing();
     }
     CloseWindow();
 }
+
